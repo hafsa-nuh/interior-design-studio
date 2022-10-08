49a980f4c24074c728e3b4d517349d2873781096
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-08 16:41:07 +0300

    subscrition

--- src/components/NewsDetails.js
+++ src/components/NewsDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const DetainsContainer = styled.div`
@@ -35,18 +35,19 @@ const FormGroup = styled.div`
   display: flex;
   position: relative;
   height: 53px;
-  margine-top: 1rem;
+  margine-top: 3rem;
   input {
     outline: #ac6b34;
     border: none;
     background-color: #1c1d24;
-    padding-left: 1.5em;
+    padding-left: 1.8em;
     padding-right: 3em;
     border-radius: 17px;
     font-size: 20px;
     height: 100% auto;
   }
   button {
+    margin-right: 4rem;
     position: absolute;
     right: -10px;
     top: 0;
@@ -69,22 +70,31 @@ const FormGroup = styled.div`
 `;
 
 function NewsDetails() {
-  return (
-    <DetainsContainer>
-      <InnerContainer>
-        <h1>We Make A New Future</h1>
-        <h3>Subscribe To Our Daily Newsletter! </h3>
-        <p>
-          We create the style of a new generation, Each of our design has
-          inventive elements that are modern and state of the art. At the same
-          time, Comforable and functional.
-        </p>
-        <FormGroup>
-          <input name="email" type="email" placeholder="Your email"/>
-          <button type="submit"> Send message</button>
-        </FormGroup>
-      </InnerContainer>
-    </DetainsContainer>
-  );
+    const [val, setVal] = useState();
+
+    return (
+      <DetainsContainer>
+        <InnerContainer>
+          <h1>We Make A New Future</h1>
+          <h3>Subscribe To Our Daily Newsletter! </h3>
+          <p>
+            We create the style of a new generation, Each of our design has
+            inventive elements that are modern and state of the art. At the same
+            time, Comforable and functional.
+          </p>
+          <FormGroup>
+            <input
+              name="email"
+              type="email"
+              value={val}
+              placeholder="Your email"
+            />
+            <button type="submit" onClick={() => setVal(() => "")}>
+              Subscribe
+            </button>
+          </FormGroup>
+        </InnerContainer>
+      </DetainsContainer>
+    );
 }
 export default NewsDetails
