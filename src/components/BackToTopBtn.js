4e4d5d92ba453b409a3de5ddc021407698ec5f26
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-08 17:36:56 +0300

    subscrition

--- src/components/BackToTopBtn.js
+++ src/components/BackToTopBtn.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Link, animateScroll as scroll} from "react-scroll"
+import {animateScroll as scroll} from "react-scroll"
 import { ChevronUpIcon } from "@heroicons/react/outline";
 
 
